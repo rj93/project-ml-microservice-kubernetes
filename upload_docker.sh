ec9fcb24b9ca99b016943ee4bf131b41aaa5c4b6
Richard Jones <contact@richardjones.io>
2022-05-26 15:48:04 +0100

    Task 4

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=rj93/ml-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u rj93 -p "$1"
+docker tag ml-app rj93/ml-app
 
 # Step 3:
 # Push image to a docker repository
+docker push rj93/ml-app
