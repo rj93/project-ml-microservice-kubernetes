92d8697fec71f9193e09a34ac9d4fb39b31f8dd2
Richard Jones <contact@richardjones.io>
2022-05-26 16:10:57 +0100

    Task 6

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=rj93/ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+pod=$(kubectl get pods -o jsonpath='{.items[*].metadata.name}')
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/"$pod" --address 0.0.0.0 8000:80
 
